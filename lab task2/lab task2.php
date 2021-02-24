15f2aa9fb165ef55e1f1e753d8d49ee6bdd7f9c7
sifatee <77955914+sifatee@users.noreply.github.com>
2021-02-24 23:19:03 +0600

    lab task2
    lab

---
+++ lab task2/lab task2.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<form ><fieldset>
+<legend>Name:</legend><input type="text" name="Name"><br>
+
+</fieldset>
+</form>
+
+
+<br>
+<form ><fieldset>
+<legend>Email:</legend><input type="text" name="Name"><br>
+
+</fieldset>
+
+</form>
+
+
+
+<br>
+<form > <fieldset>
+<legend>DATE OF BIRTH:</legend> <input type="date" id="start" name="trip-start"<br>
+</fieldset>
+</from>
+
+<br>
+<form ><fieldset>
+<legend>GENDER:</legend><input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+ 
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>S
+
+</fieldset><br
+
+</form>
+
+<fieldset>
+
+<legend>BLood Group</legend>
+<select name="Blood">
+	<option value="A+">A+</option>
+	<option value="B+">B+</option>
+	<option value="A-">A-</option>
+	<option value="B">B</option>
+	<option value="AB+">AB+</option>
+	<option value="AB-">AB-</option>
+	<option value="o+">o+</option>
+	<option value="o-">o-</option>
+
+</select>	
+<span class="error">* 
+
+	</span><hr>
+
+</fieldset><br><br>
+
+<fieldset>
+	<legend>Degree</legend>
+	
+<input type="checkbox" name="Degree"
+
+value="SSC">SSC <br>
+
+<input type="checkbox" name="Degree"
+
+value="Bsc">Bsc <br>
+
+<input type="checkbox" name="Degree"
+
+value="Msc">Msc
+
+
+
+
+</fieldset><br><br>
+<button type="submit" form="nameform" value="Submit">Submit</button>
+
+<body>
+
+</body>
+</html>
